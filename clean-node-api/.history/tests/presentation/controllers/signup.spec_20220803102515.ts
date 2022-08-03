2f74f126c005ac88ab8466d6e033782e525d4347
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-03 10:46:59 -0300

    test: ensure SignUpController return an error if no name is provided

--- clean-node-api/.history/tests/presentation/controllers/signup.spec_20220803102515.ts
+++ clean-node-api/.history/tests/presentation/controllers/signup.spec_20220803102515.ts
@@ -13,6 +13,5 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
