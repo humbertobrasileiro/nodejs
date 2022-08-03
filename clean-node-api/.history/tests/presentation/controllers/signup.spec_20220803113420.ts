6186c26fed35799f0caaab7fc75bee95899678e4
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-03 11:40:27 -0300

    refactor: add customized error for missing param

--- clean-node-api/.history/tests/presentation/controllers/signup.spec_20220803113420.ts
+++ clean-node-api/.history/tests/presentation/controllers/signup.spec_20220803113420.ts
@@ -6,6 +6,7 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
+       // name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
