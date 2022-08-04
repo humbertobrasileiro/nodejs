e9bbeacd4941cd1f5f33fac38dc2f1a42347244e
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-04 15:19:56 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- clean-node-api/tests/utils/email-validator-adapter.spec.ts
+++ clean-node-api/tests/utils/email-validator-adapter.spec.ts
@@ -1,11 +1,25 @@
 import { EmailValidatorAdapter } from '../../src/utils/email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): Boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
 
-  test('Should return false if validator retuns false', () => {
+  test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('')
+    expect(isValid).toBe(true)
   })
 
 })
