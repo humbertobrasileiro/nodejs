0ce88bcce41ab647246f4a5ad15e191e3b0c5ef7
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-04 14:38:44 -0300

    feat: ensure EmailValidatorAdapter return false if validator returns false

---
+++ clean-node-api/src/utils/email-validator.ts
@@ -0,0 +1,8 @@
+import { EmailValidator } from '../../src/presentation/protocols/email-validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return false;
+  }
+
+}
