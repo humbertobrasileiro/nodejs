5e5a8b8eca34d9eacde52a2fc34dec8d39423cb2
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-04 14:39:07 -0300

    test: ensure EmailValidatorAdapter return false if validator returns false

---
+++ clean-node-api/tests/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { EmailValidatorAdapter } from '../../src/utils/email-validator'
+
+describe('EmailValidator Adapter', () => {
+
+  test('Should return false if validator retuns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+})
