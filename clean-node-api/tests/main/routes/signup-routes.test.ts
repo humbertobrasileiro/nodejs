0835a301c96579e7d73de260000affe1c0ffe8d9
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-09 10:07:22 -0300

    test: ensure SignUp route returns an account on success

---
+++ clean-node-api/tests/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../../../src/main/config/app'
+
+describe('SignUp Routes', () => {
+
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Humberto',
+        email: 'humbertobtoscano@gmail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      })
+      .expect(200)
+  })
+})
