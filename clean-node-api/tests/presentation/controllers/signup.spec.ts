fb9958f97084c8e00c62f7e405e4f053bf5b3a53
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-10 14:38:53 -0300

    refactor: make server error return the error stack

--- clean-node-api/tests/presentation/controllers/signup.spec.ts
+++ clean-node-api/tests/presentation/controllers/signup.spec.ts
@@ -163,7 +163,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Test return 500 if EmailValidator throws'))
   })
 
   test('Should call AddAccount with correct values', async () => {
@@ -200,7 +200,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Test return 500 if AddAccount throws'))
   })
 
   test('Should return 200 if valid data is provided', async () => {
