541e6408d3c11ec01febb73e629e287770924884
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-10 18:10:33 -0300

    refactor: remove duplicated code

--- clean-node-api/src/main/factories/signup.ts
+++ clean-node-api/src/main/factories/signup.ts
@@ -5,6 +5,7 @@ import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export const makeSignUpController = (): Controller => {
 
@@ -14,6 +15,14 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const makeLogErrorRepository = (): LogErrorRepository => {
+    class LogErrorRepositoryStub implements LogErrorRepository {
+      async log (stack: string): Promise<void> {
+        return new Promise(resolve => resolve())
+      }
+    }
+    return new LogErrorRepositoryStub()
+  }
+  return new LogControllerDecorator(signUpController, makeLogErrorRepository())
 
 }
