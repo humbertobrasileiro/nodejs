d7ecdc7651d630e11c92926b4a8093fac07d8e02
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-11 13:49:13 -0300

    test: ensure LoginController returns 400 if no email is provided

---
+++ clean-node-api/tests/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,18 @@
+import { LoginController } from '../../../../src/presentation/controllers/login/login'
+import { MissingParamError } from '../../../../src/presentation/errors'
+import { badRequest } from '../../../../src/presentation/helpers/http-helper'
+
+describe('Login Controller', () => {
+
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+})
