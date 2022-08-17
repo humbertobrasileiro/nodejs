9ba0272d9633094b1a3fc8320ff6320bc888948c
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-17 11:01:11 -0300

    feat: ensure BcryptAdapter returns false when compare fails

--- clean-node-api/src/infra/criptography/bcrypt-adapter.ts
+++ clean-node-api/src/infra/criptography/bcrypt-adapter.ts
@@ -1,8 +1,9 @@
 import { rejects } from 'assert'
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/criptography/hash-comparer'
 import { Hasher } from '../../data/protocols/criptography/hasher'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
 
   private readonly salt: number
   
@@ -16,8 +17,8 @@ export class BcryptAdapter implements Hasher {
   }
 
   async compare (value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return new Promise(resolve => resolve(true))
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 
 }
