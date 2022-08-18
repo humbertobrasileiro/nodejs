1d0ed53d903549ea89678f997cf3a8240c5a985e
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-18 15:27:13 -0300

    refactor: rename signup routes with login routes

--- clean-node-api/tests/main/routes/login-routes.test.ts
+++ clean-node-api/tests/main/routes/login-routes.test.ts
@@ -19,7 +19,7 @@ describe('Login Routes', () => {
 
   describe('POST /signup', () => {
 
-    test('Should return an account on signup', async () => {
+    test('Should return 200 on signup', async () => {
       await request(app)
         .post('/api/signup')
         .send({
