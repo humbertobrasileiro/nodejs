bdb6fc70f6c55b79864b8aef7150c77708f1654e
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-18 11:27:21 -0300

    feat: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- clean-node-api/src/infra/db/mongodb/account-repository/account.ts
+++ clean-node-api/src/infra/db/mongodb/account-repository/account.ts
@@ -3,21 +3,33 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/update-access-token-repository'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData) 
-    const dados = await accountCollection.find( { _id: result.insertedId } ).toArray()
-    const index = dados.length - 1
-    return MongoHelper.map(dados[index])  
+    const data = await accountCollection.find( { _id: result.insertedId } ).toArray()
+    const index = data.length - 1
+    return MongoHelper.map(data[index])  
   }
 
   async loadByEmail (email: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({ email: email })
     return account && MongoHelper.map(account)
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({
+      _id: id
+    }, {
+      $set: {
+        accessToken: token
+      }
+    })
   }
 
 }
